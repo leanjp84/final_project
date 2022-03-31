68eb3a2df525027344fca43504e1579d89eeb20f
leanjp84 <lean84@gmail.com>
2022-03-31 17:53:07 +0200

    icons fixed

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -5,80 +5,171 @@
     >
       <thead>
         <tr>
-          <th class="px-8 py-8 bg-cyan-500 text-white">Task</th>
-          <th class="px-8 py-8 bg-cyan-500 text-white">Status</th>
-          <th class="px-8 py-8 bg-cyan-500 text-white">Actions</th>
+          <th class="px-8 py-8 bg-cyan-500 text-white text-lg text-center">
+            Pending Tasks
+          </th>
+
+          <th class="px-8 py-8 bg-cyan-500 text-white text-lg text-center">
+            Actions
+          </th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(task, index) in pendingTasks" :key="index">
-          <td>
+          <td class="text-lg">
             {{ task.title }}
           </td>
-          <td>Pending</td>
-          <td class="flex flex-row justify-center items-center">
-            <div
+          <td class="flex flex-row justify-center">
+            <button
               @click.prevent="editButton()"
-              class="w-16 border-2 border-blue-600 rounded-lg px-3 py-2 text-blue-400 cursor-pointer hover:bg-blue-600 hover:text-blue-200"
+              class="w-16 border-2 border-blue-600 rounded-lg px-4 py-1 m-2 text-blue-400 cursor-pointer hover:bg-blue-600 hover:text-blue-200"
             >
-              EDIT
-            </div>
-            <div
+              <!--edit icon -->
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                />
+              </svg>
+            </button>
+            <button
               @click.prevent="changeStatus(task)"
-              class="w-16 border-2 border-green-600 rounded-lg px-3 py-2 text-green-400 cursor-pointer hover:bg-green-600 hover:text-green-200"
+              class="w-16 border-2 border-green-600 rounded-lg px-4 py-1 m-2 text-green-400 cursor-pointer hover:bg-green-600 hover:text-green-200"
             >
-              COMPLETE
-            </div>
+              <!--complete icon -->
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
+                />
+              </svg>
+            </button>
 
-            <div
+            <button
               @click.prevent="deleteTask(task)"
-              class="w-16 border-2 border-red-600 rounded-lg px-3 py-2 text-red-400 cursor-pointer hover:bg-red-600 hover:text-red-200"
+              class="w-16 border-2 border-red-600 rounded-lg px-4 py-2 m-2 text-red-400 cursor-pointer hover:bg-red-600 hover:text-red-200"
             >
-              DELETE
-            </div>
+              <!--trash icon -->
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                />
+              </svg>
+            </button>
           </td>
         </tr>
       </tbody>
     </table>
     <div>
       <br />
+      <!--completed table -->
       <table
         class="table table-bordered mt-5 shadow-lg rounded-md table-fixed m-6 p-2"
       >
         <thead>
           <tr>
-            <th class="px-8 py-8 bg-purple-900 text-white">Task</th>
-            <th class="px-8 py-8 bg-purple-900 text-white">Status</th>
-            <th class="px-8 py-8 bg-purple-900 text-white">Actions</th>
+            <th class="px-8 py-8 bg-purple-900 text-white text-lg text-center">
+              Completed Tasks
+            </th>
+
+            <th class="px-8 py-8 bg-purple-900 text-white text-lg text-center">
+              Actions
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(task, index) in completedTasks" :key="index">
-            <td>
+            <td class="text-lg">
               {{ task.title }}
             </td>
-            <td>Completed</td>
-            <td class="flex flex-row justify-center items-center">
-              <div
+            <td class="flex flex-row justify-center">
+              <button
                 @click.prevent="editButton()"
-                class="w-16 border-2 border-blue-600 rounded-lg px-3 py-2 text-blue-400 cursor-pointer hover:bg-blue-600 hover:text-blue-200"
+                class="w-16 border-2 border-blue-600 rounded-lg px-4 py-2 m-2 text-blue-400 cursor-pointer hover:bg-blue-600 hover:text-blue-200"
               >
-                EDIT
-              </div>
-
-              <div
+                <!--edit icon -->
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                  />
+                </svg>
+              </button>
+              <br />
+              <button
                 @click.prevent="changeStatus"
-                class="w-16 border-2 border-purple-600 rounded-lg px-3 py-2 text-purple-400 cursor-pointer hover:bg-purple-600 hover:text-purple-200"
+                class="w-16 border-2 border-purple-600 rounded-lg px-4 py-2 m-2 text-purple-400 cursor-pointer hover:bg-purple-600 hover:text-purple-200"
               >
-                UPDATE
-              </div>
+                <!--update icon -->
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </button>
 
-              <div
+              <button
                 @click.prevent="deleteTask(task)"
-                class="w-16 border-2 border-red-600 rounded-lg px-3 py-2 text-red-400 cursor-pointer hover:bg-red-600 hover:text-red-200"
+                class="w-16 border-2 border-red-600 rounded-lg px-4 py-2 m-2 text-red-400 cursor-pointer hover:bg-red-600 hover:text-red-200"
               >
-                DELETE
-              </div>
+                <!--trash icon -->
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                  />
+                </svg>
+              </button>
             </td>
           </tr>
         </tbody>
@@ -96,7 +187,7 @@ export default {
     return {
       task: "",
       editedTask: null,
-      availableStatuses: ["To-do", "Donete"],
+      availableStatus: ["To-do", "Done"],
       userId: "",
       tasks: [],
       pendingTasks: [],
